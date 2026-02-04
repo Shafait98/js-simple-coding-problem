ee3c40de01cbbe8c761ec16f346cc31ea1407a34
Shafait98 <farhan98aiub@gmail.com>
2026-02-04 21:10:13 +0600

    call the calculator function and tried different parameter

--- calculator.js
+++ calculator.js
@@ -25,10 +25,25 @@ function calculator(a,b,operation)
         const result = add(a,b);
         return result;
     }
-    
+
+    else if(operation === 'subtract')
+    {
+        const result = subtract(a,b);
+        return result;
+    }
+
+    else if(operation === 'multiply')
+    {
+        const result = multiply(a,b);
+        return result;
+    }
+
     else if(operation === 'division')
     {
         const result = division(a,b);
         return result;
     }
 }
+
+const calc = calculator(20,30,'multiply');
+console.log("Result = ", calc)
